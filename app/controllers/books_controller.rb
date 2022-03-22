513a83707f539127c86355e2ddece0b97c0e2793
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-03-22 13:04:01 +0000

    [Add]bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,43 @@
 class BooksController < ApplicationController
+
+
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @bookk = Book.new
+    @user = @book.user
+  end
+
+  def destroy
+    @book = destroy.Book(params[:id])
+    @book.destroy
+    redirect_to Books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+    flash[:notice] = "successfully"
+    redirect_to user_path
+    else
+    render :edit
+    end
+  end
+
+  def users
+
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
