2167f37c0934350741fba469c3d09aec381ff4b3
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-03-22 15:10:55 +0000

    [Add]font-awesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,26 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 
-    user_path(current_user.id)
+  def after_sign_up_path_for(resource)
+     user_path(resource)
   end
 
 
   def after_sign_out_path_for(resource)
-    flash[:notice] = 'Signed out successfully.'
     root_path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+      devise_parameter_sanitizer.permit(:sign_in,keys:[:name])
   end
 
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  def redirect_login
+      redirect_to new_user_session_path unless user_signed_in?
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,43 +1,72 @@
 class BooksController < ApplicationController
-
+before_action :authenticate_user!
 
   def new
     @book = Book.new
   end
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @user = current_user
-    @books = Book.all
     if @book.save
-      flash[:notice] = "successfully"
-      redirect_to book_path(@book.id)
+       redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
-      render :index
+       @books = Book.all
+       @user = User.find(current_user.id)
+       render "index"
     end
   end
 
   def index
     @books = Book.all
-    @user = current_user
+    @user = User.find(current_user.id)
     @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-    @bookk = Book.new
+    @book_new = Book.new
     @user = @book.user
   end
 
   def destroy
-    @book = destroy.Book(params[:id])
-    @book.destroy
-    redirect_to Books_path
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(@book), method: :delete
+    flash[:notice] = "Book was successfully destroyed."
   end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+       render "edit"
+    else
+       redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+       redirect_to book_path(@book), notice:'You have updated book successfully.'
+    else
+       render "edit"
+    end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+  end
+
+
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title,:body)
   end
+
 end
