6246ba49a557253baa42a06be18b760d21de5aac
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-03-24 04:32:08 +0000

    [Add]マイグレーションファイルの修正、再提出

--- db/migrate/20220323053350_add_title_to_books.rb
+++
@@ -1,7 +0,0 @@
-class AddTitleToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :title, :string
-    add_column :books, :body, :text
-    add_column :books, :user_id, :integer
-  end
-end
